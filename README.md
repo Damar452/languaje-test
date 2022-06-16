faf2bfef56238615482221a0a477e6800e5d58f2
Cesar Romero <cesar.romero@soytul.com>
2022-06-16 09:47:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Moodle App Translation files plugin
+
+*This repository is auto-generated*
+
+Moodle App plugin including the app language strings and Behat tests. This plugin is used for translating the app strings in AMOS and running Behat tests specific to the mobile app.
+
+Notice: Tests are being moved to the App and will be removed from this repository when next version is released.
+
+More information here: https://moodle.org/mod/forum/discuss.php?d=252704
+
+https://moodle.org/plugins/view.php?plugin=local_moodlemobileapp
---
+++ lang/en/local_moodlemobileapp.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details.
+ *
+ * @package    local
+ * @subpackage moodlemobileapp
+ * @copyright  2014 Juan Leyva <juanleyvadelgado@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['appstoredescription'] = 'NOTE: This official Moodle Mobile app will ONLY work with Moodle sites that have been set up to allow it.  Please talk to your Moodle administrator if you have any problems connecting.
+
+If your Moodle site has been configured correctly, you can use this app to:
+
+- browse the content of your courses, even when offline
+- receive instant notifications of messages and other events
+- quickly find and contact other people in your courses
+- upload images, audio, videos and other files from your mobile device
+- view your course grades
+- and more!
+
+Please see http://docs.moodle.org/en/Mobile_app for all the latest information.
+
+We’d really appreciate any good reviews about the functionality so far, and your suggestions on what else you want this app to do!
+
+The app requires the following permissions:
+Record audio - For recording audio to upload to Moodle
+Read and modify the contents of your SD card - Contents are downloaded to the SD Card so you can see them offline
+Network access - To be able to connect with your Moodle site and check if you are connected or not to switch to offline mode
+Run at startup - So you receive local notifications even when the app is running in the background
+Prevent phone from sleeping - So you can receive push notifications anytime';
+$string['addon.block_myoverview.browseallcourses'] = 'Browse all courses';
+$string['addon.block_myoverview.nocoursesenrolled'] = 'You\'re not enrolled in any courses yet.';
+$string['addon.block_myoverview.nocoursesenrolleddescription'] = 'Browse all available courses below and start learning.';
+$string['addon.block_myoverview.noresult'] = 'Your search didn\'t match any courses.';
+$string['addon.block_myoverview.noresultdescription'] = 'Try adjusting your filters or browse all courses below.';
+$string['addon.blog.errorloadentries'] = 'Error loading blog entries.';
+$string['addon.blog.showonlyyourentries'] = 'Show only your entries';
+$string['addon.calendar.calendarevent'] = 'Calendar event';
+$string['addon.calendar.calendarevents'] = 'Calendar events';
+$string['addon.calendar.calendarreminders'] = 'Calendar reminders';
+$string['addon.calendar.currentmonth'] = 'Current Month';
+$string['addon.calendar.defaultnotificationtime'] = 'Default notification time';
+$string['addon.calendar.errorloadevent'] = 'Error loading event.';
+$string['addon.calendar.errorloadevents'] = 'Error loading events.';
+$string['addon.calendar.noevents'] = 'There are no events';
+$string['addon.calendar.reminders'] = 'Reminders';
+$string['addon.calendar.setnewreminder'] = 'Set a new reminder';
+$string['addon.calendar.timebefore'] = '{{value}} {{units}} before';
+$string['addon.competency.errornocompetenciesfound'] = 'No competencies found';
+$string['addon.competency.nocompetencies'] = 'No competencies';
+$string['addon.coursecompletion.complete'] = 'Complete';
+$string['addon.coursecompletion.couldnotloadreport'] = 'Could not load the course completion report. Please try again later.';
+$string['addon.messageoutput_airnotifier.processorsettingsdesc'] = 'Configure devices';
+$string['addon.messageoutput_airnotifier.pushdisabledwarning'] = 'Your users are not receiving any notification from this site on their mobile devices. Enable mobile notifications in the Notification settings page.';
+$string['addon.messages.contactlistempty'] = 'The contact list is empty';
+$string['addon.messages.contactname'] = 'Contact name';
+$string['addon.messages.deletemessage'] = 'Delete message';
+$string['addon.messages.deletemessageconfirmation'] = 'Are you sure you want to delete this message? It will only be deleted from your messaging history and will still be viewable by the user who sent or received the message.';
+$string['addon.messages.errordeletemessage'] = 'Error while deleting the message.';
+$string['addon.messages.errorwhileretrievingcontacts'] = 'Error while retrieving contacts from the server.';
+$string['addon.messages.errorwhileretrievingdiscussions'] = 'Error while retrieving discussions from the server.';
+
